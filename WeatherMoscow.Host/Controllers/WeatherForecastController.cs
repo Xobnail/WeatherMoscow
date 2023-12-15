c8adc4b21c399e67831365a6e828f5002390efc8
Nail Khabibullin <Xobnail@gmail.com>
2023-12-15 21:33:59 +0300

    add two pages for list and load forecasts

---
+++ WeatherMoscow.Host/Controllers/WeatherForecastController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherMoscow.Host.Controllers;
+
+public class WeatherForecastController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Insert()
+    {
+        return View();
+    }
+}
