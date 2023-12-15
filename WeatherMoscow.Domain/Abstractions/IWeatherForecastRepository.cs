cacb4df43dd03fad9cf1a9a532014d1eb0208e8c
Nail Khabibullin <Xobnail@gmail.com>
2023-12-15 21:31:28 +0300

    add WeatherForecastRepository

---
+++ WeatherMoscow.Domain/Abstractions/IWeatherForecastRepository.cs
@@ -0,0 +1,22 @@
+﻿using WeatherMoscow.Domain.Entities;
+
+namespace WeatherMoscow.Domain.Abstractions;
+
+/// <summary>
+/// Repository for working with the 
+/// </summary>
+public interface IWeatherForecastRepository
+{
+    /// <summary>
+    /// Gets all Weather forecasts
+    /// </summary>
+    /// <returns>IEnumerable<WeatherForecast></returns>
+    public IEnumerable<WeatherForecast> GetAll();
+
+    /// <summary>
+    /// Load Weather forecasts
+    /// </summary>
+    /// <param name="weatherForecasts">Forecast to load</param>
+    /// <returns>true if loaded, false if not</returns>
+    public bool Load(IEnumerable<WeatherForecast> weatherForecasts);
+}
---
+++ WeatherMoscow.PostgreSQL/WeatherForecastRepository.cs
@@ -0,0 +1,30 @@
+﻿using WeatherMoscow.Domain.Abstractions;
+using WeatherMoscow.Domain.Entities;
+
+namespace WeatherMoscow.PostgreSQL;
+
+/// <summary>
+/// Implementation of IWeatherForecastRepository
+/// Contains read and write methods for WeatherForecasts
+/// </summary>
+public class WeatherForecastRepository : IWeatherForecastRepository
+{
+    /// <summary>
+    /// Gets all Weather forecasts
+    /// </summary>
+    /// <returns>IEnumerable<WeatherForecast></returns>
+    public IEnumerable<WeatherForecast> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Loads Weather forecasts
+    /// </summary>
+    /// <param name="weatherForecasts">Forecast to load</param>
+    /// <returns>True if loaded, false if not</returns>
+    public bool Load(IEnumerable<WeatherForecast> weatherForecasts)
+    {
+        throw new NotImplementedException();
+    }
+}
