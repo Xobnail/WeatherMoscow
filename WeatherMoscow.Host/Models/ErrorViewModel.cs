6a325f79b8089561d7571e159a05a10419874794
Nail Khabibullin <Xobnail@gmail.com>
2023-12-15 21:32:27 +0300

    delete unused things

--- WeatherMoscow.Host/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace WeatherMoscow.Host.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
