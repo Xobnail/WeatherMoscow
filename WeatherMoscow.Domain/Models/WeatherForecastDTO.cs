c51445d36c0b3f6c5e5c8ce1961dcf6dec1ff393
Nail Khabibullin <Xobnail@gmail.com>
2023-12-15 21:33:23 +0300

    add WeatherForecastDTO

---
+++ WeatherMoscow.Domain/Models/WeatherForecastDTO.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherMoscow.Domain.Models;
+
+public class WeatherForecastDTO
+{
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public DateOnly Date { get; set; }
+    [Required]
+    public TimeOnly Time { get; set; }
+    [Required]
+    public int Temperature { get; set; }
+    [Required]
+    [Range(1, 100)]
+    public int RelativeHumidity { get; set; }
+    [Required]
+    public int DewPoint { get; set; }
+    [Required]
+    public int AtmospherePressure { get; set; }
+    [Required]
+    public string WindDirection { get; set; }
+    [Required]
+    public int WindSpeed { get; set; }
+    [Range(1, 100)]
+    public int Cloudiness { get; set; }
+    [Required]
+    public int CloudCeiling { get; set; }
+    
+    public int HorizontalVisibility { get; set; }
+    
+    public string WeatherEvents { get; set; }
+}
