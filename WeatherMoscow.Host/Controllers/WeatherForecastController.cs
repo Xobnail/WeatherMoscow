22702855972c9c2cc3ba1419c6ad718fa240bcd0
Nail Khabibullin <Xobnail@gmail.com>
2023-12-16 15:36:57 +0300

    add forecast load page

--- WeatherMoscow.Host/Controllers/WeatherForecastController.cs
+++ WeatherMoscow.Host/Controllers/WeatherForecastController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 
 namespace WeatherMoscow.Host.Controllers;
 
 public class WeatherForecastController : Controller
 {
+    private readonly IWebHostEnvironment _environment;
+ 
+    public WeatherForecastController(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+    
     public IActionResult Index()
     {
         return View();
@@ -12,5 +20,30 @@ public class WeatherForecastController : Controller
     public IActionResult Insert()
     {
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult UploadFiles(IFormFileCollection files)
+    {
+        var wwwrootPath = _environment.WebRootPath;
+ 
+        var path = Path.Combine(wwwrootPath, "Uploads");
+        
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+ 
+        foreach (var file in files)
+        {
+            var fileName = Path.GetFileName(file.FileName);
+            using (var stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+            {
+                file.CopyTo(stream);
+                ViewBag.Message += $"<b>{fileName}</b> uploaded.<br />";
+            }
+        }
+ 
+        return View("Insert");
     }
 }
