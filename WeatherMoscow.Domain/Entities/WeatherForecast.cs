1ce0c4993e2151dd872e82775f13b7217e8c7fab
Nail Khabibullin <Xobnail@gmail.com>
2023-12-18 17:21:54 +0300

    scaffolded poco classes

--- WeatherMoscow.Domain/Entities/WeatherForecast.cs
+++ WeatherMoscow.Domain/Entities/WeatherForecast.cs
@@ -4,31 +4,29 @@ namespace WeatherMoscow.Domain.Entities;
 
 public class WeatherForecast
 {
-    [Required]
     public int Id { get; set; }
-    [Required]
+
     public DateOnly Date { get; set; }
-    [Required]
+
     public TimeOnly Time { get; set; }
-    [Required]
-    public int Temperature { get; set; }
-    [Required]
-    [Range(1, 100)]
+
+    public double Temperature { get; set; }
+
     public int RelativeHumidity { get; set; }
-    [Required]
-    public int DewPoint { get; set; }
-    [Required]
+
+    public double DewPoint { get; set; }
+
     public int AtmospherePressure { get; set; }
-    [Required]
-    public string WindDirection { get; set; }
-    [Required]
-    public int WindSpeed { get; set; }
-    [Range(1, 100)]
-    public int Cloudiness { get; set; }
-    [Required]
-    public int CloudCeiling { get; set; }
-    
-    public int HorizontalVisibility { get; set; }
-    
-    public string WeatherEvents { get; set; }
+
+    public string? WindDirection { get; set; }
+
+    public int? WindSpeed { get; set; }
+
+    public int? Cloudiness { get; set; }
+
+    public int? CloudCeiling { get; set; }
+
+    public int? HorizontalVisibility { get; set; }
+
+    public string? WeatherEvents { get; set; }
 }
